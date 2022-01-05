926111c3063535a2b55f6666cf57e41fd4891b9e
Alan W. Smith <alan@alanwsmith.com>
2022-01-05 14:40:41 -0500

    Initial text on the page

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # minecraft.alanwsmith.com
-Source code for minecraft.alanwsmith.com
+
+Source code for [minecraft.alanwsmith.com](https://minecraft.alanwsmith.com)
+which is my cheatsheet of minecraft notes
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>TheMinecraftOfAlan</title>
+        <meta
+          name="description"
+          content="The Minecraft notes of Alan W. Smith"
+        />
+      </Head>
+      <main>
+        <h1>Minecraft notes</h1>
+        <ul>
+          <li>Enchanting table needs 15 book shelves around it</li>
+        </ul>
+      </main>
+    </div>
+  )
+}
