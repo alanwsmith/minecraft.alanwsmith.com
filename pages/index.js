c166533f557a067afb1a9cf3d6400a77ead05fc3
Alan Smith <alan@alanwsmith.com>
2022-03-01 21:02:15 -0500

    Added a bunch of notes

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,35 @@ export default function Home() {
         />
       </Head>
       <main>
+        <h3>
+          TODO: Add aqua affinity as one you want since it makes mining
+          underwater faster
+        </h3>
         <h1>Minecraft notes</h1>
+        <h1>Misc</h1>
+        <ul>
+          <li>
+            REMEBER: You can combine lower power books/tools into higher level
+            on an anvil
+          </li>
+        </ul>
+        <h2>Moving</h2>
+        <p>If you move, take these things:</p>
+        <ul>
+          <li>Dripstone square blocks and pointy blocks</li>
+          <li>Iron</li>
+          <li>Wool to make beds</li>
+          <li>Wood to make beds</li>
+          <li>Shears, just in case</li>
+          <li>All your tools</li>
+          <li>Boat</li>
+          <li>Torches</li>
+          <li>Charcoal to make more torches</li>
+          <li>Crafting Table</li>
+          <li>Carrots</li>
+          <li>Steak</li>
+        </ul>
+
         <h2>Enchanting Table</h2>
         <ul>
           <li>Enchanting table needs 15 book shelves around it</li>
@@ -20,6 +48,273 @@ export default function Home() {
             If you break a bookshelf without silk touch, you have to rebuild it.
           </li>
         </ul>
+        <h2>Armor</h2>
+        <ul>
+          <li>Fire Protection IV</li>
+        </ul>
+        <h2>Librarians</h2>
+        <ul>
+          <li>
+            Don&apos;t worry about getting librarians who accept papaer. Use
+            cartographers and glass instead
+          </li>
+          <li>
+            Buying lanterns and glass is a cost effective way to level
+            librarians up
+          </li>
+        </ul>
+        <h2>Portals</h2>
+        <ul>
+          <li>Cave: -2433 / 12 / -143</li>
+          <li>Neather: -313 / 63 / -7</li>
+        </ul>
+        <h2>Enchantments</h2>
+        <h3>Notes</h3>
+        <li>
+          You can get two unbreaking II and level them to III with an anvil and
+          experinece
+        </li>
+        <li>
+          Fire Protecion IV probably makes the most sense since it&apos;ll let
+          you have more of chance if you hit lava.
+        </li>
+
+        <h3>Full List</h3>
+        <p>
+          Got Power III then got two sets of two books and combined them into
+          two Power IV books for 4 experience levels each. Combied those into
+          one Power 5 book for 7 levels. So, you can take Power III to Power V
+          for 15 levels.
+        </p>
+        <p>
+          Some librarians sell bookshelfs for emeralds which can be a quick but
+          expensive way to get books
+        </p>
+        <p>Feather Falling 1 to 2 takes 2 books and 2 levels.</p>
+        <p>Feather Falling 2 to 3 takes 2 books and 5 levels</p>
+        <p>Feather Falling 3 to 4 takes 2 books and 10 levels</p>
+        <p>
+          Feather Falling 1 to 3 takes 4 books and 9 levels (check this math)
+        </p>
+        <p>
+          Feather Falling 1 to 4 takes 8 books and 28 levels (check this math)
+        </p>
+
+        <dl>
+          <dt>[x] - Aqua Affinity</dt>
+          <dt>[x] - Bane of Arthropods V</dt>
+          <dt>[x] - Blast Protection IV</dt>
+          <dt>[ ] - Channeling</dt>
+          <dt>[ ] - Curse Of Binding</dt>
+          <dt>[.] - Curse Of Vanashing</dt>
+          <dt>[ ] - Depth Strider III</dt>
+          <dt>[.] - Effificency V (currently have II)</dt>
+          <dt>[.] - Feather Falling IV (currently have I)</dt>
+          <dt>[x] - Fire Aspect II</dt>
+          <dt>[x] - Fire Protection IV</dt>
+          <dt>[x] - Flame</dt>
+          <dt>[x] - Fortune III</dt>
+          <dt>[x] - Frost Walker II</dt>
+          <dt>[ ] - Impailing V</dt>
+          <dt>[x] - Infinity</dt>
+          <dt>[x] - Knockback II</dt>
+          <dt>[x] - Looting III</dt>
+          <dt>[ ] - Loyalty III</dt>
+          <dt>[.] - Luck of the Sea III (currently have 2)</dt>
+          <dt>[.] - Lure III (currently have I)</dt>
+          <dt>[x] - Mending</dt>
+          <dt>[ ] - Multishot</dt>
+          <dt>[.] - Piercing IV (currently have 3)</dt>
+          <dt>[ ] - Projectile Protection IV</dt>
+          <dt>[.] - Protection IV (currently have 3)</dt>
+          <dt>[x] - Punch II</dt>
+          <dt>[.] - Power V (currently have 3)</dt>
+          <dt>[x] - Respiration III</dt>
+          <dt>[x] - Riptide III</dt>
+          <dt>[x] - Quick Charge III</dt>
+          <dd>Quickly reload a crossbow</dd>
+          <dt>[.] - Sharpness V (currently have IV)</dt>
+          <dt>[x] - Silk Touch</dt>
+          <dt>[ ] - Smite V</dt>{' '}
+          <dd>
+            Prefer Sharpness over Smite since it&apos;ll do everything instead
+            of just undead. E.g. culling cows
+          </dd>
+          <dt>[ ] - Soul Speed III</dt>
+          <dt>[x] - Sweeping Edge III</dt>
+          <dt>[x] - Thorns III</dt>
+          <dt>
+            [.] - Unbreaking III - (Currently have unbreaking II which can turn
+            into III for two books plus 3 levels)
+          </dt>
+        </dl>
+        <h3>Sword</h3>
+        <ul>
+          <li>Knockback II</li>
+          <li>Looting III</li>
+          <li>Mending</li>
+          <li>Sharpness V</li>
+          <li>Sweeping Edge III (tbd if it works with other stuff)</li>
+          <li>Unbreaking III</li>
+        </ul>
+        <h3>Bow</h3>
+        <ul>
+          <li>Flame</li>
+          <li>Infinity</li>
+          <li>Piercing IV (tbd if works with other stuff</li>
+          <li>Power V</li>
+          <li>Punch II</li>
+          <li>Unbreaking III</li>
+        </ul>
+        <h3>Iron Golem Farm Supplies</h3>
+        <ul>
+          <li>ladders to get down</li>
+          <li>double chest</li>
+          <li>as many buckets as you can carry for water</li>
+          <li>1 bucket of lava</li>
+          <li>4 stand alone hoppers</li>
+          <li>
+            3 hopper minecards - if you put them on track they will collide and
+            move
+          </li>
+          <li>4 pieces of standard railroad track</li>
+          <li>4 signs for the lava blade</li>
+          <li>1 stack of glass for the chamber</li>
+          <li>name tag for keep the zombie from despawning</li>
+        </ul>
+        <h3>Auto sheep shearer</h3>
+        <p>TODO: Calculate the materials so you can build on site</p>
+        <p>
+          setup a fence line with a block above the path to guide with wheat,
+          then wash in with a water bucket
+        </p>
+        <p>
+          Do not use fences to separate the sheep or you may lose some wool that
+          lands on it
+        </p>
+        <p>
+          put grass in the line in front of them too so it has more chance to
+          regrow
+        </p>
+        <ul>
+          <li>something with silk touch to move grass</li>
+          <li>1 railway track</li>
+          <li>1 chest</li>
+          <li>1 hopper for collection</li>
+          <li>1 hopper for mine cart</li>
+          <li>1 minecart</li>
+          <li>1 observer</li>
+          <li>1 dispencer</li>
+          <li>a bunch of shears</li>
+          <li>1 redstone dust</li>
+          <li>glass to put in front of pins</li>
+          <li>fence to guide sheep</li>
+          <li>bucket of water to move sheep into place</li>
+          <li>1 torch to put on block above dispencer</li>
+          <li>some wheat to lure sheep</li>
+          <li>14 glass per cube to be safe</li>
+        </ul>
+        <h3>Farm Notes</h3>
+        <ul>
+          <li>Mobs don't spawn within 24 blocks of the player</li>
+          <li>Most mobs outside 128 bocks despawn instantly</li>
+        </ul>
+        <h3>Sheep</h3>
+        <ul>
+          <li>There are 16 colors of sheep</li>
+          <li>
+            If you have these colors, you can make all of them
+            <ul>
+              <li>Black</li>
+              <li>Blue</li>
+              <li>Green</li>
+              <li>Red</li>
+              <li>Yellow</li>
+              <li>White</li>
+            </ul>
+          </li>
+        </ul>
+        <h3>Sheep Wool Colors</h3>
+        <ul>
+          <li>[x] Black - (Black dye from squid)</li>
+          <li>[x] Blue - (Dye from plant, I think)</li>
+          <li>[] Brown - ?</li>
+          <li>[] Cactus Green ?</li>
+          <li>[x] Cyan - (Blue + Green)</li>
+          <li>[x] Gray - (Black + White)</li>
+          <li>[x] Green - ?</li>
+          <li>[x] Light Blue - (Blue + White)</li>
+          <li>[x] Light Gray - (Gray + White)</li>
+          <li>[x] Lime Green - (Green + White)</li>
+          <li>[x] Magenta - (Purple + Pink)</li>
+          <li>[x] Orange - (Yellow + Red)</li>
+          <li>[x] Pink - (Red + White)</li>
+          <li>[x] Purple - (Red + Blue)</li>
+          <li>[x] Red - (Dye from flower)</li>
+          <li>[x] Yellow - (Dye from flower)</li>
+          <li>[x> White - (Defaul)</li>
+        </ul>
+        <h3>Misc</h3>
+        <ul>
+          <li>
+            Saw a note that spiders can't crawl up Soul Sand. In at least 1.18,
+            that's not true
+          </li>
+          <li>
+            Armorers and Toolsmiths will give you 12 emeralds for iron on every
+            cycle
+          </li>
+          <li>
+            Looks like Armorers and Toolsmiths charge 4 iron per emerald
+            normally and 1 per if they have been healed from being a zombie
+          </li>
+        </ul>
+        <ul>
+          <li>
+            Dark Oak saplings have to be planted in a 2x2 grid in order to grow.
+          </li>
+          <li>Bone meal turns directly into white dye</li>
+          <li>Squid ink turns directly into black dye</li>
+          <li>One white dye plus one black dye yields gray dye</li>
+          <li>One black dye plus two white dye yields light gray dye</li>
+          <li>
+            Light gray stained glass can be stacked with one air block in
+            between to make a fog effect
+          </li>
+          <li>
+            Seeds can go into a composter to make bone meal. Worth growing lots
+            of wheat for this if you want light gray tinted glass for the fog
+            effect for a big area
+          </li>
+          <li>Saplings can be composted</li>
+          <li>
+            TODO: Setup list of things that can be composted and things that
+            seem like maybe they should be able to but can&apos;t
+          </li>
+          <li>Sticks and logs can not be composted</li>
+          <li>Acacia trees are the coolest looking ones in the game</li>
+          <li>Feather will not compost</li>
+          <li>Wool will not compost</li>
+          <li>Bones wont compost but can be turned directly into bone meal</li>
+        </ul>
+        <h3>Job blocks</h3>
+        <dl>
+          <dt>Blast Furnace - Aromorer</dt>
+          <dd>
+            Trade for Iron - Use 3 smooth stone, 5 iron, and 1 furnance - when
+            you make smoth stone by smelting regular stone in a retular furance
+            it gives you the recipie
+          </dd>
+        </dl>
+        <h3>Water and Lava</h3>
+        <p>
+          If you count the source block as zero, the liquids go out and stop on
+          the seventh block
+        </p>
+        <p>
+          If you only go out six blocks, the liquid will run over and down to
+          the next level
+        </p>
       </main>
     </div>
   )
